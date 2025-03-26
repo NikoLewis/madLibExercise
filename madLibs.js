fc351f9d312881fb3d1978dc631865ceb62de2b4
Niko Lewis <nikolewis01@gmail.com>
2025-03-26 19:29:31 -0400

    added initial JavaScript variables for madLibs

--- madLibs.js
+++ madLibs.js
@@ -1,1 +1,7 @@
+const TIME_OF_DAY;
+const MATH_RESULT;
+const BOOLEAN_VALUE;
+const PERSON;
+const OBJECT_1_OWNER;
 
+let madLib = `On a stormy night, I ventured into the haunted house. The creaking walls seemed to whisper, sending chills down my spine. Suddenly, a ghost appeared, its glowing eyes piercing through the darkness. My heart raced as it floated silently toward me. Did you know 10 - 7 equals 3? Spooky, right?`
